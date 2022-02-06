88e197bcb980429c20a26ed38d0c6437c93b4b4d
skywal1ker <ik11@bu.edu>
2022-02-06 11:23:32 +0600

    HomeWork#3 is Completed and Updated

--- python assignments/#3/HW3_1.py
+++ python assignments/#3/HW3_1.py
@@ -1,9 +1,9 @@
 """1. How many three-digit numbers are divisible by 17? Write a program to print them."""
 
-divis = 17
+divs = 17
 
 for number in range(100,900):
-    if number % divis ==0:
+    if number % divs ==0:
         print(number)
 
 
