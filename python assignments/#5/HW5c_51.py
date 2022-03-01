0cabde8884ecbadcd1fefb2db697a47f00e9332e
skywal1ker <ik11@bu.edu>
2022-03-01 11:29:32 +0600

    HomeWork# is Completed and Updated

--- python assignments/#5/HW5c_51.py
+++ python assignments/#5/HW5c_51.py
@@ -2,10 +2,9 @@
 51. Write your own versions of the Python built-in functions min and max. They should
 take a list as an argument and return the minimum or maximum element. Hint: Pick
 the first element as the minimum (maximum) and then loop through the elements to
-find a smaller (arger) element. Each time you find a smaller larger) element, update
+find a smaller (arguer) element. Each time you find a smaller larger) element, update
 your minimum (maximum).
 """
-
 
 def min(lst):
     m=lst[0]
@@ -20,7 +19,6 @@ def max(lst):
         if lst[element]>m:
             m=lst[element]
     return m
-
 
 print("Min = ",min([10,20,3,56]))
 print("Max = ",max([10,20,3,56]))
